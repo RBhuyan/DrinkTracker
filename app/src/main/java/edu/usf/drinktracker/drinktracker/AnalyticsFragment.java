d818e1596f492aa4df17d0418359be335e108048
Autumn Chavez <amchavez088@gmail.com>
2019-03-24 20:24:55 -0400

    Graph Added w/ temp points & UI for analytics implemented

--- app/src/main/java/edu/usf/drinktracker/drinktracker/AnalyticsFragment.java
+++ app/src/main/java/edu/usf/drinktracker/drinktracker/AnalyticsFragment.java
@@ -5,6 +5,7 @@ Will contain whatever analytic function we invoke to show your drinking history
 package edu.usf.drinktracker.drinktracker;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,6 +15,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
 
 
 public class AnalyticsFragment extends Fragment {
@@ -27,13 +33,6 @@ public class AnalyticsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
-        });
-        graph.addSeries(series);*/
     }
 
     @Override
@@ -45,6 +44,37 @@ public class AnalyticsFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+            GraphView graph = (GraphView) getActivity().findViewById(R.id.graph);
+            PointsGraphSeries<DataPoint> series = new PointsGraphSeries<>(new DataPoint[] { // these points are a test will add more points after each session.
+                    new DataPoint(1, .08),
+                    new DataPoint(2, .11),
+                    new DataPoint(3, .05)
+            });
+            graph.setTitle("Average BAC Per Session");
+            graph.setTitleTextSize(75);
+            graph.getGridLabelRenderer().setHorizontalAxisTitle("Sessions");
+            graph.getGridLabelRenderer().setPadding(60);
+            //graph.getGridLabelRenderer().setVerticalAxisTitle("BAC %");
+            graph.setBackgroundColor(Color.DKGRAY);
+            graph.getGridLabelRenderer().setHumanRounding(false, false);
+            graph.getGridLabelRenderer().setNumHorizontalLabels((int)series.getHighestValueX());
+            graph.getGridLabelRenderer().setNumVerticalLabels(6);
+            //graph.getSecondScale().setMinY(0.0);
+            //graph.getSecondScale().setMaxY(0.15);
+            graph.addSeries(series);
+            graph.getViewport().setXAxisBoundsManual(true);
+            graph.getViewport().setMaxX(series.getHighestValueX());
+            graph.getViewport().setYAxisBoundsManual(true);
+            graph.getViewport().setMaxY(0.15);
+            graph.getViewport().setMinY(0.0);
+
+            //add if more than 1 session make graph visible
+
+            //on data change for sessions, add new point for new session.
+
+
+        }
     }
 
-}
+
